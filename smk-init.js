38eb865b197fdf022f1c9f88548843c6b5a104e5
CCOSSETT <carlo.cossette@gov.bc.ca>
2026-01-13 13:55:39 -0800

    Initial Commit

---
+++ smk-init.js
@@ -0,0 +1,45 @@
+SMK.INIT( {
+    containerSel: '#smk-map-frame',
+    config: [ './smk-config.json', './config/route-planner.json', '?' ]
+} )
+.then( function ( smk ) {
+    // SMK initialized
+    window.L_DISABLE_3D = true;
+
+    var dialog = document.getElementById( 'disclaimer' )
+    include( [ { url: './assets/disclaimer.html' } ], 'disclaimer' ).then( function ( inc ) {
+        dialog.innerHTML = inc[ 'disclaimer.disclaimer-html' ]
+        dialogPolyfill.registerDialog(dialog)
+        dialog.showModal()
+    } )
+
+    SMK.HANDLER.set('DirectionsRouteTool', 'print', function(smk, tool, key, opt) {
+        if ( opt.debug ) {
+            window.open( 'print-directions-portrait.html?' + key, '', [
+                "location=yes",
+                "menubar=yes",
+                "titlebar=yes",
+                "status=yes",
+                "toolbar=yes",
+                "scrollbars=yes",
+            ].join( ',' ) );
+        }
+        else {
+            document.getElementById( 'print-holder' ).src = 'print-directions-portrait.html?' + key;
+        }
+        
+        return new Promise( function ( res, rej ) {
+            var handler = function ( ev ) {
+                console.log( ev );
+                if ( ev.data == 'printed' ) {
+                    res();
+                    window.removeEventListener( 'message', handler );
+                }
+            };
+
+            window.addEventListener( 'message', handler, false );
+
+            setTimeout( function () { rej( new Error( 'timeout' ) ) }, 30 * 1000 );
+        });
+    });
+} )
